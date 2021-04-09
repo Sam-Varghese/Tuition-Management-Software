0cf733ecb38b06a8f19b62f0c6c3df6ee16d9662
Sam Varghese <sam.varghese.sica@gmail.com>
2021-04-09 10:02:28 +0530

    removed cursor from geogebra.py

--- Tuition_Management_Software/Extra_Softwares/GeoGebra.py
+++ Tuition_Management_Software/Extra_Softwares/GeoGebra.py
@@ -19,51 +19,51 @@ def geogebra():
     
     # Making button for Calculator Suite
     
-    geo_win_b1=ttk.Button(geo_win_lf1, text='Calculator Suite', cursor='wait')
+    geo_win_b1=ttk.Button(geo_win_lf1, text='Calculator Suite')
     geo_win_b1.grid(row=0, column=0, padx=5, pady=5)
     
     # Making button for 3D Calculator 
     
-    geo_win_b2=ttk.Button(geo_win_lf1, text='3D Calculator', cursor='wait')
+    geo_win_b2=ttk.Button(geo_win_lf1, text='3D Calculator')
     geo_win_b2.grid(row=0, column=1, padx=5, pady=5)
     
     # Making button for CAS Calculator
     
-    geo_win_b3=ttk.Button(geo_win_lf1, text='CAS Calculator', cursor='wait')
+    geo_win_b3=ttk.Button(geo_win_lf1, text='CAS Calculator')
     geo_win_b3.grid(row=0, column=2, padx=5, pady=5)
     
     # Making button for Geometry
     
-    geo_win_b4=ttk.Button(geo_win_lf1, text='Geometry', cursor='wait')
+    geo_win_b4=ttk.Button(geo_win_lf1, text='Geometry')
     geo_win_b4.grid(row=1, column=0, padx=5, pady=5)
     
     # Making button for Graphing Calculator
     
-    geo_win_b5=ttk.Button(geo_win_lf1, text='Graphing Calculator', cursor='wait')
+    geo_win_b5=ttk.Button(geo_win_lf1, text='Graphing Calculator')
     geo_win_b5.grid(row=1, column=1, padx=5, pady=5)
     
     # Making button for Scientific Calculator
     
     geo_win_b6 = ttk.Button(
-        geo_win_lf1, text='Scientific Calculator', cursor='wait')
+        geo_win_lf1, text='Scientific Calculator')
     geo_win_b6.grid(row=1, column=2, padx=5, pady=5)
 
     # Making button for GeoGebra Classic
 
     geo_win_b7 = ttk.Button(
-        geo_win_lf1, text='GeoGebra Classic', cursor='wait')
+        geo_win_lf1, text='GeoGebra Classic')
     geo_win_b7.grid(row=2, column=0, padx=5, pady=5)
  
     # Making button for Testing
 
     geo_win_b8 = ttk.Button(
-        geo_win_lf1, text='Testing', cursor='wait')
+        geo_win_lf1, text='Testing')
     geo_win_b8.grid(row=2, column=1, padx=5, pady=5)
     
     # Making button for Notes
 
     geo_win_b9 = ttk.Button(
-        geo_win_lf1, text='Notes', cursor='wait')
+        geo_win_lf1, text='Notes')
     geo_win_b9.grid(row=2, column=2, padx=5, pady=5)
 
     geo_win.mainloop()
